0aa2c31ae5287027f3bc7cb287e4abf8b511239f
BrunoJay <brunojay001@gmail.com>
2022-08-13 01:52:46 +0300

    update fixes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -286,9 +286,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "bac885ad458bb74a15dbcbacf5235584",
-"/": "bac885ad458bb74a15dbcbacf5235584",
-"main.dart.js": "871afbb462b00ffa372c57754257bf25",
+"index.html": "f9816d81ef7ec0602a1270897178c9a0",
+"/": "f9816d81ef7ec0602a1270897178c9a0",
+"main.dart.js": "6fdfa7769b144bf99752c60182ba7d8f",
 "manifest.json": "a909f625edeb7c6b8834945702befcdd",
 "version.json": "0db956a33cfeebc06af07c74b6583245"
 };
